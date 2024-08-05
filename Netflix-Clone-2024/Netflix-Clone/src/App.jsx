7d7a3278d7fb64d19ea8016b39777bf282c8ca81
Samrawitgebremaryam <samrawitgebremaryam121@gmail.com>
2024-08-05 12:08:15 +0300

    [feat]: Netflix clone added

---
+++ Netflix-Clone-2024/Netflix-Clone/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './App.css'
+import Home from './Pages/Home/Home.jsx'
+import "./index.css"
+function App() {
+  return (
+    <div>
+    <Home/>
+    </div>
+  )
+}
+
+export default App
